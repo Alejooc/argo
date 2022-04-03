7545768220dfb0050feb3f5c7412b1e3b6b0dc1c
Alejandro osorio <engoomadonpixeles@gmail.com>
2022-04-03 13:45:09 -0500

    testeando

--- app/Views/pages/landing.php
+++ app/Views/pages/landing.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="<?=base_url()?>/assets/css/index.css">
-    <title>ArgoCMS</title>
+    <title>ArgoCMS - F</title>
 </head>
 <body>
     <section class="px-4 text-white slota shadow-sm">
